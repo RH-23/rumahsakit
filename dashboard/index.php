91836228c1f29544f6f7911f5518e70215d59255
rizkihariyanto23 <rizkihariyanto23@gmail.com>
2020-02-17 13:44:31 +0700

    merubah tombol toggle menu pada dashboard file index.html

--- dashboard/index.php
+++ dashboard/index.php
@@ -4,7 +4,7 @@
         <div class="col-lg-12">
             <h1>Dashboard</h1>
             <p>Selamat Datang <mark><?=$_SESSION['user'];?></mark> di website Rumah Sakit Sari Asih (Rekam Medis)</p>
-            <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
+            <a href="#menu-toggle" class="btn btn-primary" id="menu-toggle">Toggle Menu</a>
         </div>
     </div>
 
